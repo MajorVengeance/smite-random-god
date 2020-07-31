ba70d32b3f39dd781a725a3d9da8c578af93e082
Ben Martin <ben_m365@hotmail.co.uk>
2020-07-31 13:44:57 +0100

    got my packages working?

---
+++ .node_scripts/build/build.sh
@@ -0,0 +1,7 @@
+cp config.json dist/config.json
+cp gods.json dist/gods.json
+cp package.json dist/package.json
+cp index.js dist/index.js
+cp -r commands dist/commands
+cp -r data dist/data
+cp -r node_modules dist/node_modules
